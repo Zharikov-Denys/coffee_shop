3dce0bb2731d1fa01ac7efd6b8dacfc083144c1a
Denis <denjor75@gmali.com>
2022-02-28 23:02:58 +0200

    Users app. Updated error messages in the UserManager manager.

--- users/managers.py
+++ users/managers.py
@@ -3,9 +3,17 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
+    create_user_errors = {
+        'email field is required': _('The email field is required.'),
+    }
+    create_superuser_errors = {
+        'is_staff field is required': _('The is_staff field have to be set to True for a superuser.'),
+        'is_superuser field is required': _('The is_superuser field have to be set to True for a superuser.'),
+    }
+
     def create_user(self, email: str, password: str, **extra_fields):
         if not email:
-            raise ValueError(_('The email field is required.'))
+            raise ValueError(self.create_user_errors['email field is required'])
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -18,7 +26,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_active', True)
 
         if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('The is_staff field have to be set to True for a superuser.'))
+            raise ValueError(self.create_superuser_errors['is_staff field is required'])
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('The is_superuser field have to be set to True for a superuser.'))
+            raise ValueError(self.create_superuser_errors['is_superuser field is required'])
         return self.create_user(email, password, **extra_fields)
