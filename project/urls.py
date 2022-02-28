ab8b3325af0893eddd7f9751788442c0fc283577
Denis <denjor75@gmali.com>
2022-02-28 21:17:30 +0200

    Created the users app.

--- project/urls.py
+++ project/urls.py
@@ -14,13 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+api_url_patterns = [
+    path('api/', include([
+        path('users/', include('users.api.urls')),
+    ])),
+]
+
+
+urlpatterns += api_url_patterns
 
 
 if settings.DEBUG:
