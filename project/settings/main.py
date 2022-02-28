cc39ad3413866ecdebc1fa60ea59966e3fdcb7a4
Denis <denjor75@gmali.com>
2022-02-28 22:33:00 +0200

    Configured static and media files.

--- project/settings/main.py
+++ project/settings/main.py
@@ -123,12 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_dev')]
+STATIC_ROOT = os.path.join(PROJECT_ROOT_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(PROJECT_ROOT_DIR, 'static_dev')]
 
 # Media files (files uploaded by users)
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
